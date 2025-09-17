3e9259a50cc2ede1d6b5da046b8c4dbf8b43ce26
triadynata <triadynata.zamhur@gmail.com>
2025-09-17 21:56:22 +0700

    add uuidArray

--- models/list_position.go
+++ models/list_position.go
@@ -1,10 +1,13 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"github.com/triadynata/project-management/models/types"
+)
 
 type ListPosition struct {
-	InternalID int64     `json:"internal_id" db:"internal_id" gorm:"primaryKey;autoIncrement"`
-	PublicID   uuid.UUID `json:"public_id" db:"public_id" gorm:"public_id"`
-	BoardID    int64     `json:"board_internal_id" db:"board_internal_id" gorm:"column:board_internal_id"`
-	//ListOrder  UUIDArray //array uuid {uuid1,uuid2}
+	InternalID int64           `json:"internal_id" db:"internal_id" gorm:"primaryKey;autoIncrement"`
+	PublicID   uuid.UUID       `json:"public_id" db:"public_id" gorm:"public_id"`
+	BoardID    int64           `json:"board_internal_id" db:"board_internal_id" gorm:"column:board_internal_id"`
+	ListOrder  types.UUIDArray `json:"list_order"`
 }
--- models/types/uuid_array.go
+++ models/types/uuid_array.go
@@ -1,7 +1,10 @@
 package types
 
 import (
+	"database/sql/driver"
 	"errors"
+	"fmt"
+	"strings"
 
 	"github.com/google/uuid"
 )
@@ -9,6 +12,8 @@ import (
 type UUIDArray []uuid.UUID
 
 func (a *UUIDArray) Scan(value interface{}) error {
+
+	//{213123asdad,asdasda,123123}
 	var str string
 
 	switch v := value.(type) {
@@ -17,7 +22,45 @@ func (a *UUIDArray) Scan(value interface{}) error {
 	case string:
 		str = v
 	default:
-		return errors.New("Gagal parsing UUIDArray : Unsupported data type")
+		return errors.New("failed to parse UUIDArray: unsupport data type")
 	}
+
+	str = strings.TrimPrefix(str, "{")
+	str = strings.TrimSuffix(str, "}")
+	parts := strings.Split(str, ",")
+
+	//make([]T,length , capacity)
+	*a = make(UUIDArray, 0, len(parts))
+	for _, s := range parts {
+		s = strings.TrimSpace(strings.Trim(s, `"`)) // akan menghapus spasi dan "
+		if s == "" {
+			continue
+		}
+		u, err := uuid.Parse(s)
+		if err != nil {
+			return fmt.Errorf("invalid UUID in Array : %v", err)
+		}
+		*a = append(*a, u)
+
+	}
+	return nil
+
+}
+
+//{"550e8400-e29b-41d4-a716-446655440000","123e4567-e89b-12d3-a456-426614174000"}
+
+func (a UUIDArray) Value() (driver.Value, error) {
+	if len(a) == 0 {
+		return "{}", nil
+	}
+	postgreFormat := make([]string, 0, len(a))
+	for _, value := range a {
+		postgreFormat = append(postgreFormat, fmt.Sprintf(`"%s"`, value.String()))
+	}
+	return "{" + strings.Join(postgreFormat, ",") + "}", nil
+
+}
+
+func (UUIDArray) GormDataType() string {
 
 }
